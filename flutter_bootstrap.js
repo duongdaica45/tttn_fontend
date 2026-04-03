8f7c8891ae777b08d70aaeb5f93a609397340760
duongdaica45 <ntduong.ti@gmail.com>
2026-04-03 06:15:02 +0000

    Deploying to gh-pages from @ duongdaica45/tttn_fontend@36c430f007370e5232e7a2a8df57a217f7745936 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2966225024" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "4252926154" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
